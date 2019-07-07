47ba97617bb54a441adc11767086f47befdf2ade
tier777 <tier1993@gmail.com>
2019-07-07 05:01:43 +0300

    Basic design implemented.

---
+++ ImagesTest/ImagesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImagesListViewController.swift
+//  ImagesTest
+//
+//  Created by Nikita Gorobets on 07/07/2019.
+//  Copyright © 2019 ng. All rights reserved.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
